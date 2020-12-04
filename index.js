2059695870b7278f5271a0ed507e9ccbb2ebba5b
FgSurewin <mrliu3244794@gmail.com>
2020-12-03 20:32:34 -0500

    fix port problem

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const nunjucks = require("nunjucks");
 
 const app = express();
-const PORT = process.env.PORT | 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static("./public"));
 
